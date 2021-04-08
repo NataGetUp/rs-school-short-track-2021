32bffdfbe906e63cae2dcf8c27a051e4a4d29b7a
natagetup <nata.getup@gmail.com>
2021-04-08 17:28:41 +0300

    feat: add solution for matrix-elements-sum task

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,24 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let count = 0;
+  const excludeCounts = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let k = 0; k < matrix[i].length; k++) {
+      if (matrix[i][k] === 0) {
+        excludeCounts.push(k);
+      } else {
+        const isInclude = excludeCounts.includes(k);
+
+        if (isInclude === false) {
+          count += matrix[i][k];
+        }
+      }
+    }
+  }
+  return count;
 }
 
 module.exports = getMatrixElementsSum;
