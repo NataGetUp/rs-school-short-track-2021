57f8b9a0f769d2650891e358053129c6b7489683
natagetup <nata.getup@gmail.com>
2021-04-09 16:13:25 +0300

    feat: add solution for encode-line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let newStr = '';
+  let accNum = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    while (str[i] === str[i + 1]) {
+      accNum += 1;
+      i++;
+    }
+
+    if (accNum === 1) {
+      newStr += str[i];
+    } else {
+      newStr += accNum + str[i];
+      accNum = 1;
+    }
+  }
+
+  return newStr;
 }
 
 module.exports = encodeLine;
